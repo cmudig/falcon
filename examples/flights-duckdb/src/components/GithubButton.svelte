b9d707c7163aa883601107c63158c84a13d5be6f
xnought <bertuccd@oregonstate.edu>
2023-05-24 22:40:13 -0700

    fix: link github mark instead of importing it

--- examples/flights-duckdb/src/components/GithubButton.svelte
+++ examples/flights-duckdb/src/components/GithubButton.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import Logo from "../assets/github-mark.svg";
-
 	export let href: string = "";
 	export let width: number = 25;
 </script>
 
 <a {href} target="_blank">
-	<img src={Logo} alt="github logo" {width} />
+	<img src="github-mark.svg" alt="github logo" {width} />
 </a>
 
 <style>
